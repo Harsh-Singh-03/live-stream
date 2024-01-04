508063bebbee78b0d9774bf065909601078b284c
Harsh Singh <harshnov802@gmail.com>
2024-01-04 13:28:57 +0530

    fix dependencies

--- components/Header/Actions.tsx
+++ components/Header/Actions.tsx
@@ -51,7 +51,7 @@ const Actions = ({ isDashboard, user }: props) => {
             return (() => setUserData({ status: "authenticated", user: user }))
         }
 
-    })
+    },[])
 
     return (
         <div className="flex items-center justify-end gap-x-2">
